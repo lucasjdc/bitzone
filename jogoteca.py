61c28bcfdfef30ddc1e1583ced01c38bca447777
lucasjdc <lucasjpdc@gmail.com>
2025-06-16 16:08:08 -0300

    Criar um novo jogo

--- jogoteca.py
+++ jogoteca.py
@@ -9,12 +9,14 @@ class Jogo:
 app = Flask(__name__)
 
 @app.route('/inicio')
-
 def ola():
-    jogo1 = Jogo('Tomb Raider: Atlantean Scion','Ação-aventura','Sega Saturn')
+    jogo1 = Jogo('Metroid Prime','Ação-aventura','GameCube')
     jogo2 = Jogo('The Legend of Zelda','Ação-aventura','Nintendo')
     jogo3 = Jogo('Mortal Kombat','Luta','PS2')
     lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
+@app.route('/novo')
+def novo():
+    return render_template('novo.html',titulo='Novo Jogo')
 app.run()
