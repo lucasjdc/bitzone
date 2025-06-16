dc862b11d095f2f6c1a38df0f2b577a161df3fee
lucasjdc <lucasjpdc@gmail.com>
2025-06-16 15:47:32 -0300

    Mostrando atributos na view

--- jogoteca.py
+++ jogoteca.py
@@ -11,9 +11,10 @@ app = Flask(__name__)
 @app.route('/inicio')
 
 def ola():
-    jogo1 = Jogo('Treris','Puzzle','Atari')
-    jogo2 = Jogo('God of War','Rack n Slash','PS2')
-    lista = [jogo1,jogo2]
+    jogo1 = Jogo('Tomb Raider: Atlantean Scion','Ação-aventura','Sega Saturn')
+    jogo2 = Jogo('The Legend of Zelda','Ação-aventura','Nintendo')
+    jogo3 = Jogo('Mortal Kombat','Luta','PS2')
+    lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
