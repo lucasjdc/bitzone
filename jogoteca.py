b344cc3d3bea535ccfe9fee610c97601e552ff5d
lucasjdc <lucasjpdc@gmail.com>
2025-06-16 16:38:20 -0300

    Resolvendo um POST no servidor

--- jogoteca.py
+++ jogoteca.py
@@ -13,15 +13,15 @@ jogo2 = Jogo('The Legend of Zelda','Ação-aventura','Nintendo')
 jogo3 = Jogo('Mortal Kombat','Luta','PS2')
 lista = [jogo1, jogo2, jogo3]
 
-@app.route('/inicio')
-def ola():    
+@app.route('/')
+def index():    
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 @app.route('/novo')
 def novo():
     return render_template('novo.html',titulo='Novo Jogo')
 
-@app.route('/criar')
+@app.route('/criar', methods=['POST',])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
@@ -29,4 +29,4 @@ def criar():
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
     return render_template('lista.html', titulo='Jogos', jogos=lista)
-app.run()
+app.run(debug=True)
