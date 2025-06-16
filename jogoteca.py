1b3fd667ca6a757ed54849c8c527d8e9296fe785
lucasjdc <lucasjpdc@gmail.com>
2025-06-16 15:34:00 -0300

    Deixando mais orientado a objetos

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 
 def ola():
-    lista = ['Duck Tales', 'Super Mario Bros 2', 'Metroid', 'Zelda', 'Mega Man 2' ]
+    jogo1 = Jogo('Treris','Puzzle','Atari')
+    jogo2 = Jogo('God of War','Rack n Slash','PS2')
+    lista = [jogo1,jogo2]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
