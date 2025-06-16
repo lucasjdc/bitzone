17d2c4e70413ef38e938b7fa1ab730a830f030f8
lucasjdc <lucasjpdc@gmail.com>
2025-06-16 15:22:32 -0300

    Pegando lista do servidor

--- jogoteca.py
+++ jogoteca.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/inicio')
 
 def ola():
-    return render_template('lista.html', titulo='Jogos')
+    lista = ['Duck Tales', 'Super Mario Bros 2', 'Metroid', 'Zelda', 'Mega Man 2' ]
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
